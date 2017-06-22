2a815b55b76afc2f423f5d9a571960c2f61a4b97
Marian Hello <marian.hello@gmail.com>
2017-06-22 11:06:05 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿// https://stackoverflow.com/questions/30880709/c-sharp-native-host-with-chrome-native-messaging
+// https://social.msdn.microsoft.com/Forums/en-US/92846ccb-fad3-469a-baf7-bb153ce2d82b/simple-udp-example-code?forum=netfxnetcom
+
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static System.Console;
+
+namespace chrome_native_messaging_host
+{
+   class Program
+   {
+      public static void Main(string[] args)
+      {
+         Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+         IPAddress address = IPAddress.Parse("127.0.0.1");
+         EndPoint endPoint = new IPEndPoint(address, 19700);
+
+         Console.Error.WriteLine("[INFO] Messaging host listening...");
+
+         JObject json;
+         while ((json = Read()) != null)
+         {
+            Console.Error.WriteLine("[DEBUG] msg received: {0}", json.ToString());
+            if (json["action"] != null);
+            {
+                  try 
+                  {
+                        byte[] sendBuffer = Encoding.ASCII.GetBytes(json.ToString());
+                        socket.SendTo(sendBuffer, endPoint);
+                        byte[] receiveBuffer = new byte[4096];
+                        socket.ReceiveFrom(receiveBuffer, ref endPoint);
+                        var response = Encoding.ASCII.GetString(receiveBuffer);
+                        Console.Error.WriteLine("[DEBUG]' socket received {0}", response);
+                        Write(response);
+                  } catch (Exception e)
+                  {
+                        Console.Error.WriteLine("[ERROR] socket send: {0}", e.ToString());
+                  }
+
+            }
+         }
+      }
+
+      public static JObject Read()
+      {
+         var stdin = Console.OpenStandardInput();
+         var length = 0;
+
+         var lengthBytes = new byte[4];
+         stdin.Read(lengthBytes, 0, 4);
+         length = BitConverter.ToInt32(lengthBytes, 0);
+
+         var buffer = new char[length];
+         using (var reader = new StreamReader(stdin))
+         {
+            while (reader.Peek() >= 0)
+            {
+               reader.Read(buffer, 0, buffer.Length);
+            }
+         }
+
+         return (JObject)JsonConvert.DeserializeObject<JObject>(new string(buffer));
+      }
+
+      public static void Write(String jsonString)
+      {
+         var json = (JObject)JsonConvert.DeserializeObject<JObject>(jsonString);
+         var bytes = System.Text.Encoding.UTF8.GetBytes(json.ToString(Formatting.None));
+
+         var stdout = Console.OpenStandardOutput();
+         stdout.WriteByte((byte)((bytes.Length >> 0) & 0xFF));
+         stdout.WriteByte((byte)((bytes.Length >> 8) & 0xFF));
+         stdout.WriteByte((byte)((bytes.Length >> 16) & 0xFF));
+         stdout.WriteByte((byte)((bytes.Length >> 24) & 0xFF));
+         stdout.Write(bytes, 0, bytes.Length);
+         stdout.Flush();
+      }
+   }
+}
---
+++ README.md
@@ -0,0 +1,80 @@
+# Chrome Native Messaging Host for KeePassXC (aka keepassxc-proxy)
+
+Self-contained app intented to act as proxy between Google Chrome browser and KeePassXC app
+
+All dotnet runtime libraries included.
+*More info: https://github.com/dotnet/dotnet-docker-samples/tree/master/dotnetapp-selfcontained
+
+
+## Create app binary
+Binaries can be created for multiple runtimes/platforms supported by dotnet core.
+
+Supported runtimes:
+
+* centos.7-x64;
+* debian.8-x64
+* ol.7.0-x64
+* ol.7.1-x64
+* ol.7.2-x64
+* opensuse.13.2-x64
+* osx.10.10-x64
+* osx.10.11-x64
+* linuxmint.17-x64
+* linuxmint.17.1-x64
+* linuxmint.17.2-x64
+* linuxmint.17.3-x64
+* rhel.7.2-x64
+* ubuntu.14.04-x64
+* ubuntu.14.10-x64
+* ubuntu.15.04-x64
+* ubuntu.15.10-x64
+* ubuntu.16.04-x64
+* win7-x64
+* win7-x86]
+
+All possible runtimes can be found in  [chrome_native_messaging_host.csproj](chrome_native_messaging_host.csproj).
+
+Run docker container for building app:
+
+    docker run -it -v $(pwd):/native_messaging_host microsoft/dotnet
+
+From running container:
+
+    cd /native_messaging_host
+
+Build binary with selected runtime ubuntu.16.04-x64:
+
+    dotnet publish -c Release -o out -r ubuntu.16.04-x64
+
+Binary can now be accesible from docker host:
+    
+    ./out/chrome_native_messaging_host
+
+## Register Google Chrome native messaging host
+
+Following snippet will register native messaging host. Adjust accordingly.
+
+    CHROME_NATIVE_HOSTS_PATH="${HOME}/.config/google-chrome/NativeMessagingHosts"
+
+    mkdir -p $CHROME_NATIVE_HOSTS_PATH
+
+    PROXY_PATH="$(pwd)/out/chrome_native_messaging_host"
+    cat << EOF > "${CHROME_NATIVE_HOSTS_PATH}/com.varjolintu.keepassxc_browser.json" && echo DONE
+    {
+      "name": "com.varjolintu.keepassxc_browser",
+      "description": "KeepassXC integration with Chrome with Native Messaging support",
+      "path" : "${PROXY_PATH}",
+      "type": "stdio",
+      "allowed_origins": [
+        "chrome-extension://iopaggbpplllidnfmcghoonnokmjoicf/",
+        "chrome-extension://fhakpkpdnjecjfceboihdjpfmgajebii/"
+      ]
+    }
+    EOF
+
+## Debug
+
+* Start Chrome in Terminal. All logging is done to stderr.
+* Monitor traffic with tcpdump
+>
+    tcpdump -i lo 'port 19700' -vv -A
